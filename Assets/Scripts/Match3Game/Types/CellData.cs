23490d709a982e30177eb52ece21524e17cdb106
n-fridman <n.fridman@goto-apps.com>
2021-09-08 23:47:37 +0300

    Create cell data

---
+++ Assets/Scripts/Match3Game/Types/CellData.cs
@@ -0,0 +1,12 @@
+namespace Match3Game.Types
+{
+    /// <summary>
+    /// Cell data
+    /// </summary>
+    [System.Serializable]
+    public struct CellData
+    {
+        public int scoreCount;
+        public CellType type;
+    }
+}
