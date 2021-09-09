f083bc4011fca038bc090ffa5af843feedb5970c
n-fridman <n.fridman@goto-apps.com>
2021-09-09 22:31:06 +0300

    Create cell presenter

---
+++ Assets/Scripts/Match3Game/Board/CellPresenter.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Match3Game.Board
+{
+    [AddComponentMenu("Match 3 Game/Board/Cell Presenter")]
+    public class CellPresenter : MonoBehaviour
+    {
+        [Header("Components")] 
+        [SerializeField] private Animator _cellAnimator;
+        [SerializeField] private Image _cellImage;
+        [SerializeField] private Text _cellScoreText;
+       
+        private static readonly int Destroy1 = Animator.StringToHash("Destroy");
+
+        private void Awake()
+        {
+            if (_cellAnimator == null) _cellAnimator = GetComponent<Animator>();
+            if (_cellImage == null) _cellImage = GetComponent<Image>();
+            if (_cellScoreText == null) _cellScoreText = GetComponentInChildren<Text>();
+        }
+
+        /// <summary>
+        /// Set cell color.
+        /// </summary>
+        /// <param name="color">Cell color.</param>
+        public void SetCellColor(Color color) => _cellImage.color = color;
+        
+        /// <summary>
+        /// Set cell score.
+        /// </summary>
+        /// <param name="score">Cell score.</param>
+        public void SetCellScore(int score) => _cellScoreText.text = score.ToString();
+        
+        /// <summary>
+        /// Play cell destroy animation.
+        /// </summary>
+        public void PlayDestroyAnimation() => _cellAnimator.SetTrigger(Destroy1);
+    }
+}
