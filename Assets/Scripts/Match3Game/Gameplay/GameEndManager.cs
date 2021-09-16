95dfaa9357a8f87b7e6c8b884a362f9e1ba78e0a
n-fridman <n.fridman@goto-apps.com>
2021-09-16 11:22:35 +0300

    Update game end manager

--- Assets/Scripts/Match3Game/Gameplay/GameEndManager.cs
+++ Assets/Scripts/Match3Game/Gameplay/GameEndManager.cs
@@ -1,18 +1,23 @@
-using System;
+using Match3Game.Raitings;
+using Match3Game.SceneManagement;
 using Match3Game.Types;
 using Match3Game.UI;
 using UnityEngine;
 
 namespace Match3Game.Gameplay
 {
-    public class LoseManager : MonoBehaviour
+    public class GameEndManager : MonoBehaviour
     {
         [Header("Components")] 
         [SerializeField] private LosePopup _losePopup;
-
+        [SerializeField] private RaitingsManager _raitingsManager;
+        [SerializeField] private SceneLoader _loader;
+        
         private void Awake()
         {
             if (_losePopup == null) _losePopup = FindObjectOfType<LosePopup>(true);
+            if (_raitingsManager == null) _raitingsManager = FindObjectOfType<RaitingsManager>();
+            if (_loader == null) _loader = FindObjectOfType<SceneLoader>();
         }
 
         /// <summary>
@@ -21,7 +26,14 @@ namespace Match3Game.Gameplay
         /// <param name="result">Game result data.</param>
         public void OnGameLose(GameResult result)
         {
-            _losePopup.ShowPopup();
+            if (_raitingsManager.AddGameResult(result))
+            {
+                _loader.LoadScene("Raitings");
+            }
+            else
+            {
+                _losePopup.ShowPopup();
+            }
         }
     }
 }
