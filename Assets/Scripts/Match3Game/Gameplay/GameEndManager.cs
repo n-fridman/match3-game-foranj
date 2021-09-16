fb0d77aaa4d0aa89df0afa999230c279432f9395
n-fridman <n.fridman@goto-apps.com>
2021-09-16 15:35:48 +0300

    Update game end manager

--- Assets/Scripts/Match3Game/Gameplay/GameEndManager.cs
+++ Assets/Scripts/Match3Game/Gameplay/GameEndManager.cs
@@ -2,6 +2,7 @@ using Match3Game.Raitings;
 using Match3Game.SceneManagement;
 using Match3Game.Types;
 using Match3Game.UI;
+using Match3Game.Utils;
 using UnityEngine;
 
 namespace Match3Game.Gameplay
@@ -10,6 +11,7 @@ namespace Match3Game.Gameplay
     {
         [Header("Components")] 
         [SerializeField] private LosePopup _losePopup;
+        [SerializeField] private PauseManager _pauseManager;
         [SerializeField] private RaitingsManager _raitingsManager;
         [SerializeField] private SceneLoader _loader;
         
@@ -18,13 +20,14 @@ namespace Match3Game.Gameplay
             if (_losePopup == null) _losePopup = FindObjectOfType<LosePopup>(true);
             if (_raitingsManager == null) _raitingsManager = FindObjectOfType<RaitingsManager>();
             if (_loader == null) _loader = FindObjectOfType<SceneLoader>();
+            if (_pauseManager == null) _pauseManager = FindObjectOfType<PauseManager>();
         }
 
         /// <summary>
         /// On game lose event handler.
         /// </summary>
         /// <param name="result">Game result data.</param>
-        public void OnGameLose(GameResult result)
+        public void OnGameEnd(GameResult result)
         {
             if (_raitingsManager.AddGameResult(result))
             {
@@ -32,6 +35,7 @@ namespace Match3Game.Gameplay
             }
             else
             {
+                _pauseManager.SetPause();
                 _losePopup.ShowPopup();
             }
         }
